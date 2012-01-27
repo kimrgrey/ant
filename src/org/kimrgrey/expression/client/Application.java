21e9e9cc64c89617347c1ef0b7b9392a7b76c4f7
Sergey Tsvetkov <sergey.tsvetkov@accenture.com>
2012-01-27 15:41:04 +0400

    Первый вариант шаблонного проекта.

---
+++ src/org/kimrgrey/expression/client/Application.java
@@ -0,0 +1,15 @@
+package org.kimrgrey.expression.client;
+
+public class Application {
+    public static void main(String... args) {   
+        Application application = new Application(args);
+        application.execute();
+    }
+    
+    public Application(String... args) {
+    }
+    
+    public void execute() {
+        
+    }
+}
